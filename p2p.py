6dcb80dc05a219cba1616d50eea4c49c0a1a60bd
Alex... Pi <randomiseme33@gmail.com>
2017-08-10 06:13:47 +1000

    Update p2p.py

--- p2p.py
+++ p2p.py
@@ -8,18 +8,18 @@ from termcolor import colored
 
 def main():
     while True:
-        l = input('Choose an action:\n1 | Chat\n2 | Exit\n')
+        l = int(input('Choose an action:\n1 | Chat\n2 | Exit\n'))
         if l == 1:
             chat()
         elif l == 2:
             sys.exit()
         else:
-            print "Not valid.\n"
+            print("Not valid.\n")
 
 def chat():
-    ip = raw_input("Enter an IP address: ")
+    ip = input("Enter an IP address: ")
 
-    print "Connecting..."
+    print("Connecting...")
     
     punch(ip)
     
@@ -33,8 +33,8 @@ def chat():
 def punch(host):
 
     p = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    p.bind(("", port))
-    p.sendto("", (host, port))
+    p.bind((b'', port))
+    p.sendto(b'', (host, port))
     p.close()
 
 def keyExchange(s, host):
@@ -56,30 +56,30 @@ def server(host, s):
     Random.atfork()
     connected = False
 
-    s.sendto("", (host, port))
+    s.sendto(b"", (host, port))
 
     while True:
         data = s.recv(1024)
         if connected == False:
-            print "Connected to %s\n" % host
-            s.sendto("", (host, port))
+            print("Connected to %s\n" % host)
+            s.sendto(b"", (host, port))
             connected = True
         elif data == "":
             pass
         else:
             dec = privKey.decrypt(data)
             if dec == "exit":
-                print "User disconnected. Type 'exit' to exit."
+                print("User disconnected. Type 'exit' to exit.")
                 break
             else:
-                print colored("%s: "% host, "red") + dec
+                print(colored("%s: "% host, "red") + dec.decode("UTF-8"))
 
 def client(host, s, hostPubKey, serverProcess):
 
     while True:
-        m = raw_input()
-        enc = hostPubKey.encrypt(m, 32)[0]
-        s.sendto(str(enc), (host, port))
+        m = str(input())
+        enc = hostPubKey.encrypt(m.encode("UTF-8"), 32)[0]
+        s.sendto(enc, (host, port))
         if m == "exit":
             serverProcess.terminate()
             s.close()
@@ -89,10 +89,8 @@ if __name__ == "__main__":
 
     port = 6311
     HOME = os.environ['HOME']
-
     keys = pickle.load(open(HOME + "/.p2py/keys", "rb"))
     privKey = RSA.importKey(keys[0])
     pubKey = RSA.importKey(keys[1])
 
     main()
-
